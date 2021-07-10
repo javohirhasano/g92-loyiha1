0e709e9d5593b594292cd982d03fa32bbd7a5ce8
javohirhasano <ijavoxir5@gmail.com>
2021-07-10 14:58:37 +0500

    'commentariya'

---
+++ loyiha1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container ">
+      <div className="card">
+        <img className="mb-3" rsc="https://i.morioh.com/2020/03/09/e383809dba47.jpg"></img>
+        <h1 className="m-3">John Doe </h1>
+        <p>lorem ipsum</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
