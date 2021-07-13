45c2b4edcdf76c47ee72a28493d6840cb7f458be
javohirhasano <ijavoxir5@gmail.com>
2021-07-13 15:17:35 +0500

    tittle changed

--- loyiha1/src/App.js
+++ loyiha1/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container ">
       <div className="card">
-        <img className="mb-3" rsc="https://i.morioh.com/2020/03/09/e383809dba47.jpg"></img>
+        <img className="mb-3" rsc=""></img>
         <h1 className="m-3">John Doe </h1>
         <p>lorem ipsum</p>
       </div>
